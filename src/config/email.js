75f9401a777f118d8c6208d332509ba482660307
Nwafor Valentine Kenechukwu <nwaforvalentinekenechukwu@Nwafors-MacBook-Pro.local>
2026-06-01 22:35:32 +0100

    Add: added the email config

---
+++ src/config/email.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter object using SMTP transport
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: Number(process.env.SMTP_PORT) === 465,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    },
+});
+
+module.exports = transporter;
